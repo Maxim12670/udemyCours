26f756a507013f745a91c4a3f94f3369090cafde
Максим <akaikinmax@mail.ru>
2023-09-20 22:48:12 +0700

    feature(main.js): выделение числовой части при помощи регулярного выражения для параметра width

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -308,11 +308,15 @@ window.addEventListener('DOMContentLoaded', function() {
         slide.style.width = width;
     });
 
+    function deleteNotDigits(str){
+        return +str.replace(/\D/g, "");
+    }
+
     next.addEventListener('click', () => {
-        if(offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if(offset == deleteNotDigits(width) * (slides.length - 1)){
             offset = 0;
         }else{
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         if(slideIndex == slides.length){
@@ -332,9 +336,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
     prev.addEventListener('click', () => {
         if(offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         }else{
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         if(slideIndex == 1){
